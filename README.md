b78dc3a9a0cec70b76e21791949ff9c2a40a9b06
Udo Wuertz <uwuertz@pop-os.localdomain>
2020-05-14 09:44:10 +0200

    Upload 14052020

--- README.md
+++
@@ -1,4 +0,0 @@
-# nvjetson_opencv_gsi
-Installation von OpenCV und Yolov3 auf dem NVIDIA Jetson Nano 
-
-Set-Up in progress. Wird in den nächsten Tagen online stehen
--- nvidia2sudoers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ "$(whoami)" != "root" ]; then
-        echo "Script muss als root ausgeführt werden!"
-        exit 255
-fi
-sudo cp /etc/sudoers /etc/sudoers.bak
-sudo echo 'nvidia ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
