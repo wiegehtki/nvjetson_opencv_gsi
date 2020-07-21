1c43e43ce2f7cb75c051981c9c993aaa62be4d56
root <root@pop-os.localdomain>
2020-07-21 08:38:20 +0200

    nvidia Benutzer in sudoers Gruppe

--- nvidia2sudoers.sh
+++ nvidia2sudoers.sh
@@ -4,5 +4,6 @@ if [ "$(whoami)" != "root" ]; then
         echo "Script muss als root ausgeführt werden!"
         exit 255
 fi
+
 sudo cp /etc/sudoers /etc/sudoers.bak
 sudo echo 'nvidia ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
