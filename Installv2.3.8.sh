18e9f7ee36da0b36372c3bd84536ee749b3cb59b
root <root@pop-os.localdomain>
2020-12-01 11:27:39 +0100

    Update Install-Script mit JP4.4.1 Support - ReleaseCandidate 0.2

--- Installv2.3.8.sh
+++ Installv2.3.8.sh
@@ -220,27 +220,27 @@ echo $(date -u) "...............................................................
                  sudo apt-get update
                  sudo apt-get install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran
                  sudo apt-get install python3-pip
-                 sudo pip3 install -U pip testresources setuptools==49.6.0               
+                 sudo -H pip3 install -U pip testresources setuptools==49.6.0               
                 
-                 sudo pip3 install -U numpy==1.16.1 future==0.18.2 mock==3.0.5 h5py==2.10.0 keras_preprocessing==1.1.1 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
+                 sudo -H pip3 install -U numpy==1.16.1 future==0.18.2 mock==3.0.5 h5py==2.10.0 keras_preprocessing==1.1.1 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
                  
                  # Letzte Version von Tensorflow installieren #
                  
                  # sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v44 tensorflow
                  
                  # Letzte Version 1.x von tensorflow installieren #
-                 sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v44 ‘tensorflow<2’
+                 sudo -H pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v44 ‘tensorflow<2’
 
                  sudo apt -y install cython-doc
                  sudo apt -y install cython3
-                 sudo pip3 install -U cython==0.29.21
+                 sudo -H pip3 install -U cython==0.29.21
                  sudo apt -y install cython
                  sudo apt -y install python3-matplotlib
-                 sudo pip3 install -U pycocotools==2.0.2
-                 sudo pip3 install -U scipy==1.4.1
-                 sudo pip3 install -U keras==2.3.1
-                 sudo pip3 install -U grpcio==1.28.1 absl-py==0.9.0 py-cpuinfo==5.0.0 psutil==5.7.0 portpicker==1.3.1 six==1.14.0 requests==2.23.0 
-                 sudo pip3 install -U astor==0.8.1 termcolor==1.1.0 wrapt==1.12.1 google-pasta==0.2.0  
+                 sudo -H pip3 install -U pycocotools==2.0.2
+                 sudo -H pip3 install -U scipy==1.4.1
+                 sudo -H pip3 install -U keras==2.3.1
+                 sudo -H pip3 install -U grpcio==1.28.1 absl-py==0.9.0 py-cpuinfo==5.0.0 psutil==5.7.0 portpicker==1.3.1 six==1.14.0 requests==2.23.0 
+                 sudo -H pip3 install -U astor==0.8.1 termcolor==1.1.0 wrapt==1.12.1 google-pasta==0.2.0  
 
 #                 sudo pip3 install -U h5py==2.10.0 astor==0.8.1 termcolor==1.1.0 wrapt==1.12.1 google-pasta==0.2.0 setuptools==49.6.0 testresources==2.0.1 
 #                 sudo pip3 install -U pybind11==2.5.0
@@ -248,7 +248,7 @@ echo $(date -u) "...............................................................
                  export CUDA_VISIBLE_DEVICES=0
                  export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/lib
                  export PATH=$PATH:/usr/local/cuda/bin
-                 sudo pip3 install --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v43 tensorflow-gpu==1.15.0+nv20.1
+                 sudo -H pip3 install --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v43 tensorflow-gpu==1.15.0+nv20.1
 
                  echo 'export CUDA_VISIBLE_DEVICES=0' >> ~/.bashrc
                  echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/lib' >> ~/.bashrc
