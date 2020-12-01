d3c5c6287a62cfb519bc84e614e1b5763d8504f2
root <root@pop-os.localdomain>
2020-12-01 12:04:15 +0100

    Update Install-Script mit JP4.4.1 Support - ReleaseCandidate 0.3

--- Installv2.3.8.sh
+++ Installv2.3.8.sh
@@ -222,14 +222,15 @@ echo $(date -u) "...............................................................
                  sudo apt-get install python3-pip
                  sudo -H pip3 install -U pip testresources setuptools==49.6.0               
                 
-                 sudo -H pip3 install -U numpy==1.16.1 future==0.18.2 mock==3.0.5 h5py==2.10.0 keras_preprocessing==1.1.1 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
+                
+                 sudo -H pip3 install -U numpy==1.16.1 future==0.18.2 mock==3.0.5 h5py==2.10.0 keras_preprocessing==1.1.1 keras_applications==1.0.8 gast==0.3.3 futures protobuf pybind11
                  
                  # Letzte Version von Tensorflow installieren #
                  
                  # sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v44 tensorflow
                  
                  # Letzte Version 1.x von tensorflow installieren #
-                 sudo -H pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v44 ‘tensorflow<2’
+                 sudo -H pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v44 'tensorflow<2'
 
                  sudo apt -y install cython-doc
                  sudo apt -y install cython3
