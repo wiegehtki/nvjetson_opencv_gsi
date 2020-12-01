ac174890148dc570b0aae3b29ea2acc09e72b2dd
root <root@pop-os.localdomain>
2020-12-01 12:06:44 +0100

    Update Install-Script mit JP4.4.1 Support - ReleaseCandidate 0.3

--- Installv2.3.8.sh
+++ Installv2.3.8.sh
@@ -225,13 +225,6 @@ echo $(date -u) "...............................................................
                 
                  sudo -H pip3 install -U numpy==1.16.1 future==0.18.2 mock==3.0.5 h5py==2.10.0 keras_preprocessing==1.1.1 keras_applications==1.0.8 gast==0.3.3 futures protobuf pybind11
                  
-                 # Letzte Version von Tensorflow installieren #
-                 
-                 # sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v44 tensorflow
-                 
-                 # Letzte Version 1.x von tensorflow installieren #
-                 sudo -H pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v44 'tensorflow<2'
-
                  sudo apt -y install cython-doc
                  sudo apt -y install cython3
                  sudo -H pip3 install -U cython==0.29.21
@@ -249,7 +242,14 @@ echo $(date -u) "...............................................................
                  export CUDA_VISIBLE_DEVICES=0
                  export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/lib
                  export PATH=$PATH:/usr/local/cuda/bin
-                 sudo -H pip3 install --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v43 tensorflow-gpu==1.15.0+nv20.1
+                 #sudo -H pip3 install --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v43 tensorflow-gpu==1.15.0+nv20.1
+                 #####################################################
+                 # Letzte Version V2.x.x von Tensorflow installieren #
+                 # sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v44 tensorflow
+                 
+                 # Letzte Version 1.x von tensorflow installieren    #
+                 sudo -H pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v44 'tensorflow<2'
+                 #####################################################
 
                  echo 'export CUDA_VISIBLE_DEVICES=0' >> ~/.bashrc
                  echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/lib' >> ~/.bashrc
