a8530ca631f937e3c65e7ec57980d8b681cb7433
wiegehtki.de <58086193+wiegehtki@users.noreply.github.com>
2021-01-09 14:33:04 +0100

    Update Installv2.3.9.sh

--- Installv2.3.9.sh
+++ Installv2.3.9.sh
@@ -2,7 +2,7 @@
 
 
 # Es wird empfohlen nvidia als Benutzer zu verwenden da leider einige Softwarepakete diesen hardcodiert enthalten (können) und man sucht dann lange nach Fehlern
-export Benutzer="nvidia"
+Benutzer="nvidia"
 
 
 # Hier kann die Tensorflow - Version gesetzt werden, welche installiert werden soll.
@@ -19,7 +19,7 @@ fi
 set +x
 
 # Name der virtuellen Umgebung, kann nach persönlichem Geschmack problemlos geändert werden
-export VirtEnv="tf15cv4"
+VirtEnv="tf15cv4"
 
 #Vorbelegung CompilerFlags und Warnungen zu unterdrücken die durch automatisch generierten Code schnell mal entstehen können und keine wirkliche Relevanz haben
 export CFLAGS=$CFLAGS" -w"
@@ -76,11 +76,12 @@ echo $(date -u) "...............................................................
 echo $(date -u) "04 von 30: Swap-File mit 8GB anlegen, aktivieren und kontrollieren"  | tee -a  ~/Installation.log
 echo $(date -u) "....................................................................................................................................." | tee -a  ~/Installation.log
                  sudo apt -y install nano
+                 #sudo swapoff /swapfile
                  sudo fallocate -l 8G /swapfile
                  sudo chmod 600 /swapfile
                  sudo mkswap /swapfile
                  sudo swapon /swapfile
-                 sudo swapon --show
+                 sudo swapon --show | tee -a  ~/Installation.log
 
 echo $(date -u) "05 von 30: Systemwerkzeuge, Editor, Tools, Erweiterungen"  | tee -a  ~/Installation.log
 echo $(date -u) "....................................................................................................................................." | tee -a  ~/Installation.log
@@ -114,12 +115,12 @@ echo $(date -u) "10 von 30: WIEGEHTKI Repo-clone"  | tee -a  ~/Installation.log
 echo $(date -u) "....................................................................................................................................." | tee -a  ~/Installation.log
                  cd ~
                  git clone https://github.com/wiegehtki/nvjetson_opencv_gsi.git
-		 chmod +x ~/nvjetson_opencv_gsi/*
+                 chmod +x ~/nvjetson_opencv_gsi/*
                  mv ~/nvjetson_opencv_gsi/darknet.repo ~/darknet
-		 mv ~/nvjetson_opencv_gsi/smartcam.sh  ~/darknet/
-		 cp -r ~/nvjetson_opencv_gsi/Objekterkennung/Videos_Ausgabe ~/darknet/
-		 cp -r ~/nvjetson_opencv_gsi/Objekterkennung/Videos_Eingabe ~/darknet/
-		 chmod +x ~/darknet/*
+                 mv ~/nvjetson_opencv_gsi/smartcam.sh  ~/darknet/
+                 cp -r ~/nvjetson_opencv_gsi/Objekterkennung/Videos_Ausgabe ~/darknet/
+                 cp -r ~/nvjetson_opencv_gsi/Objekterkennung/Videos_Eingabe ~/darknet/
+                 chmod +x ~/darknet/*
 
 echo $(date -u) "11 von 30: PreCompiler: cmake 3.19.0"  | tee -a  ~/Installation.log
 echo $(date -u) "....................................................................................................................................." | tee -a  ~/Installation.log
@@ -147,11 +148,12 @@ echo $(date -u) "...............................................................
                  rm get-pip.py
                  sudo apt -y install python3-pip
                  python3 -m pip install pip testresources setuptools
+                 chown -R nvidia:nvidia /home/nvidia/.cache
 
 echo $(date -u) "14 von 30: Virtuelle Umgebung installieren, anpassen und aktivieren"  | tee -a  ~/Installation.log
 echo $(date -u) "....................................................................................................................................." | tee -a  ~/Installation.log
                  cd ~
-                 python3 -m pip virtualenv virtualenvwrapper
+                 sudo apt -y install virtualenv virtualenvwrapper
                  export WORKON_HOME=$HOME/.virtualenvs
                  export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
 
@@ -159,6 +161,7 @@ echo $(date -u) "...............................................................
                  echo 'export WORKON_HOME=$HOME/.virtualenvs' >> ~/.bashrc
                  echo 'export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3' >> ~/.bashrc
                  echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc
+                 echo 'source /home/nvidia/.local/bin/virtualenvwrapper.sh' >>~/.bashrc
 
                  set +e
                  eval "$(cat ~/.bashrc | tail -n +3)"
@@ -192,27 +195,27 @@ echo $(date -u) "...............................................................
                  sudo apt-get update
                  sudo apt-get install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran
                  sudo apt-get install python3-pip
-                 python3 -m pip install pip testresources setuptools==49.6.0               
+                 python3 -m pip install pip testresources setuptools==49.6.0
                  python3 -m pip install numpy==1.16.1 future==0.18.2 mock==3.0.5 h5py==2.10.0 keras_preprocessing==1.1.1 keras_applications==1.0.8 gast==0.3.3 futures protobuf pybind11
                  sudo apt -y install cython3
                  sudo apt -y install python3-matplotlib
                  python3 -m pip install --ignore-installed pycocotools==2.0.2
                  python3 -m pip install scipy==1.4.1
                  python3 -m pip install keras==2.3.1
-                 python3 -m pip install grpcio absl-py==0.9.0 py-cpuinfo==5.0.0 psutil==5.7.0 portpicker==1.3.1 six==1.14.0 requests==2.23.0 
-                 python3 -m pip install astor==0.8.1 termcolor==1.1.0 wrapt==1.12.1 google-pasta==0.2.0  
+                 python3 -m pip install grpcio absl-py==0.9.0 py-cpuinfo==5.0.0 psutil==5.7.0 portpicker==1.3.1 six==1.14.0 requests==2.23.0
+                 python3 -m pip install astor==0.8.1 termcolor==1.1.0 wrapt==1.12.1 google-pasta==0.2.0
 
                  export CUDA_VISIBLE_DEVICES=0
                  export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/lib
                  export PATH=$PATH:/usr/local/cuda/bin
-                 
+
                  if [ $TensorFlow = "1" ]
                  then
                           python3 -m pip --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v44 'tensorflow<2'
                  else
                           python3 -m pip --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v44 tensorflow
                  fi
-                
+
                  echo 'export CUDA_VISIBLE_DEVICES=0' >> ~/.bashrc
                  echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/lib' >> ~/.bashrc
                  echo 'export PATH=$PATH:/usr/local/cuda/bin' >> ~/.bashrc
@@ -278,7 +281,7 @@ echo $(date -u) "...............................................................
 
 echo $(date -u) "22 von 30: OpenCV herunterladen, compilieren und installieren"  | tee -a  ~/Installation.log
 echo $(date -u) "....................................................................................................................................." | tee -a  ~/Installation.log
-                 cmake	-D CMAKE_BUILD_TYPE=RELEASE \
+                 cmake  -D CMAKE_BUILD_TYPE=RELEASE \
                         -D OPENCV_GENERATE_PKGCONFIG=YES \
                         -D WITH_CUDA=ON \
                         -D CUDA_ARCH_PTX="" \
